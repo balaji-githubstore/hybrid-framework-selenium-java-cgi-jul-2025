75a537e56f9741d005547efdd1d48476fb94dc15
balaji-githubstore <balajidinakaran1@gmail.com>
2025-07-10 06:29:57 +0000

    Added Before and AfterMethod For Browser Config

--- src/test/java/com/cgi/test/LoginUITest.java
+++ src/test/java/com/cgi/test/LoginUITest.java
@@ -6,36 +6,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginUITest {
 
-	@Test
-	public void titleTest()
-	{
-		WebDriver driver=new ChromeDriver();
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setup() {
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));	
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://opensource-demo.orangehrmlive.com/");
-		
-		String actualTitle= driver.getTitle();
+	}
+
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+
+	@Test
+	public void titleTest() {
+		String actualTitle = driver.getTitle();
 		Assert.assertEquals(actualTitle, "OrangeHRM");
 	}
-	
+
 	@Test
-	public void headerTest()
-	{
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));	
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		
-		//Assert the header - Login
-		String actualHeader= driver.findElement(By.xpath("//h5")).getText();
+	public void headerTest() {
+		// Assert the header - Login
+		String actualHeader = driver.findElement(By.xpath("//h5")).getText();
 		Assert.assertEquals(actualHeader, "Login");
 	}
 }
-
-
-
-
